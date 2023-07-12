2920c09fc3f4eee273a369081301b2e596e28bc7
PankajKumar <113239726+Pankajkumar6204@users.noreply.github.com>
2023-07-12 15:02:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+window.addEventListener('load',() =>{
+    const form = document.querySelector('#task-form');
+    const input = document.querySelector('#new-task-input');
+    const listElement = document.querySelector('#tasks');
+
+
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault();
+
+        const task = input.value;
+
+        if(!task){
+            alert('Please Fill The Task!');
+            return;
+        }
+
+        const taskElement = document.createElement('div');
+        taskElement.classList.add('task');
+
+        const taskContentElement = document.createElement('div');
+        taskContentElement.classList.add('content');
+        // taskContentElement.innerText = task;
+
+        taskElement.appendChild(taskContentElement);
+        
+        const taskInputElement = document.createElement('input');
+        taskInputElement.classList.add('text');
+        taskInputElement.type = 'text';
+        taskInputElement.value = task;
+        taskInputElement.setAttribute('readonly','readonly');
+
+        taskContentElement.appendChild(taskInputElement);
+
+        const taskActionsElement = document.createElement('div');
+        taskActionsElement.classList.add('actions');
+
+        // listElement.appendChild(taskElement);
+
+        const taskEditElement = document.createElement('button');
+        taskEditElement.classList.add('edit');
+        taskEditElement.innerHTML = 'Edit';
+
+        const taskDeleteElement = document.createElement('button');
+        taskDeleteElement.classList.add('delete');
+        taskDeleteElement.innerHTML = 'Delete';
+
+        taskActionsElement.appendChild(taskEditElement);
+        taskActionsElement.appendChild(taskDeleteElement);
+
+        taskElement.appendChild(taskActionsElement);
+
+        listElement.appendChild(taskElement);
+
+        input.value = "";
+
+        taskEditElement.addEventListener('click',() =>{
+            if(taskEditElement.innerText.toLowerCase() == 'edit'){
+                taskInputElement.removeAttribute('readonly');
+                taskInputElement.focus();
+                taskEditElement.innerText = 'save';
+            }
+            else{
+                taskInputElement.setAttribute('readonly','readonly');
+                taskEditElement.innerText = 'edit';
+            }
+        });
+
+        taskDeleteElement.addEventListener('click',() =>{
+           listElement.removeChild(taskElement);
+
+        });
+    });
+});
